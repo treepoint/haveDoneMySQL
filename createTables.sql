30862236f744355ac6f4b815edc857696e03d768
Paul Khoziashev <owner-of-all-real@ya.ru>
2019-12-01 19:51:52 +0500

    add categories and tasks

--- createTables.sql
+++ createTables.sql
@@ -4,3 +4,23 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3;
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(400) NOT NULL,
+  `description` varchar(4000) NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (user_id)  REFERENCES users (id)
+) ENGINE=InnoDB  DEFAULT CHARSET=UTF8;
+
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `name` varchar(400) NOT NULL,
+  `description` varchar(4000) NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (user_id)  REFERENCES users (id),
+    FOREIGN KEY (category_id)  REFERENCES categories (id)
+) ENGINE=InnoDB  DEFAULT CHARSET=UTF8;
